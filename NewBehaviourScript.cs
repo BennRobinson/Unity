544645b4754b475810396b1b0cf34eecde735f7c
BennRobinson <53872902+BennRobinson@users.noreply.github.com>
2021-02-09 13:35:39 +0000

    Add files via upload

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+     public float movementSpeed = 10;
+  
+     void Update(){
+  
+        transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+  
+     }
+ }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
